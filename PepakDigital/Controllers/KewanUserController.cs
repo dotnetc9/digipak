e4546ed1e042e7ca5c059fea06016aac37c7f67d
Anggarda Diajeng Prameswari <ajprameswari@gmail.com>
2014-06-14 11:57:05 +0700

    view fix - untested yet

--- PepakDigital/Controllers/KewanUserController.cs
+++ PepakDigital/Controllers/KewanUserController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PepakDigital;
+using PagedList;
 
 namespace PepakDigital.Controllers
 {
@@ -17,8 +18,27 @@ namespace PepakDigital.Controllers
         // GET: /KewanUser/
         public ActionResult Index()
         {
+        //    ViewBag.CurrentSortOrder = sortData;
+          //  ViewBag.SortingName = String.IsNullOrEmpty(sortData) ? "Urut_Kewan" : "";
             var kewan = db.Kewan.Include(k => k.Kategori);
             return View(kewan.ToList());
+            /*
+            switch (sortData)
+            {
+                case "Urut_Kewan":
+                    kewan = kewan.OrderByDescending(hewan => hewan.JenengKewan);
+                    break;
+                default:
+                    kewan = kewan.OrderBy(hewan => hewan.JenengKewan);
+                    break;
+            }
+
+            int Size_Of_Page = 10;
+            int No_Of_Page = (Page_No ?? 1);
+            return View(kewan.ToPagedList(No_Of_Page, Size_Of_Page));
+            */
         }
+
+            
     }
 }
